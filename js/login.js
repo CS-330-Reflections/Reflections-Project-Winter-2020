0782933d3157b3e4d014da8fb1c81c7947b94207
ptarasul18 <paulina.tarasul@gmail.com>
2020-03-04 16:40:39 -0600

    Redirected login

--- js/login.js
+++ js/login.js
@@ -4,11 +4,11 @@ function login() {
     if (uname == users[0]["uname"] && pwd == users[0]["pwd"]) {
         localStorage.setItem("profile", profiles[0]["profile"]);
         localStorage.setItem("entries", profiles[0]["entries"]);
-        window.location.href = "/index.html";
+        window.location.href = "/nicole.html";
     } else if (uname == users[1]["uname"] && pwd == users[1]["pwd"]) {
         localStorage.setItem("profile", profiles[1]["profile"]);
         localStorage.setItem("entries", profiles[1]["entries"]);
-        window.location.href = "/index.html";
+        window.location.href = "/andres.html";
     } else {
         // Need to error
     }
