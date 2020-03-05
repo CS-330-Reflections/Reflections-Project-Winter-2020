e3b22fe9d6deb40c5b624fb1b4493bbb1f54a109
Hakan Dingenc <hakan@u.northwestern.edu>
2020-03-04 22:20:24 -0600

    Reorder hard-coded entries

--- js/profiles.js
+++ js/profiles.js
@@ -6,7 +6,19 @@ profiles = [{
         bio: "mother of two <3 love hiking!! Have two puppers, work in banking! live laugh",
         goals: "reflect twice a week!!"
     },
-    "entries": [{"date":"2020-03-01","mood":["happy"],"visual":"","prompt":"free_entry","entry":"First day of March! Such a warm day, I took the kids out to the park without their winter coats."},{"date":"2020-03-03","mood":["happy", "confident"],"visual":"","prompt":"free_entry","entry":"Got colder again, but what else can you expect from March? Work's going well, though, and the kids went to sleep with hardly any fuss, so I have time to write now."}]
+    "entries": [{
+        "date": "2020-03-03",
+        "mood": ["happy", "confident"],
+        "visual": "",
+        "prompt": "free_entry",
+        "entry": "Got colder again, but what else can you expect from March? Work's going well, though, and the kids went to sleep with hardly any fuss, so I have time to write now."
+    }, {
+        "date": "2020-03-01",
+        "mood": ["happy"],
+        "visual": "",
+        "prompt": "free_entry",
+        "entry": "First day of March! Such a warm day, I took the kids out to the park without their winter coats."
+    }]
 }, {
     "profile": {
         name: "Andres",
@@ -15,5 +27,35 @@ profiles = [{
         bio: "hello",
         goals: "reflect every day!!"
     },
-    "entries": [{"date":"2020-01-01","mood":["apprehensive"],"visual":"","prompt":"free_entry","entry":"Happy New Year! Going back to school soon."},{"date":"2020-02-04","mood":[],"visual":"","prompt":"free_entry","entry":"Went to a move night today"},{"date":"2020-03-01","mood":["happy"],"visual":"","prompt":"free_entry","entry":"It was sunny today, and I went for a walk. It's nice to feel winter passing. Maybe I'll get out more now."},{"date":"2020-03-03","mood":["sad"],"visual":"","prompt":"free_entry","entry":"Got a C on my test. Spent an hour at the gym trying to work off  the frustration. I'll do better next time."},{"date":"2020-03-04","mood":["annoyed"],"visual":"","prompt":"prompt3","entry":"The guys next door keep playing their music too loud. I need to study!"}]
+    "entries": [{
+        "date": "2020-03-04",
+        "mood": ["annoyed"],
+        "visual": "",
+        "prompt": "prompt3",
+        "entry": "The guys next door keep playing their music too loud. I need to study!"
+    }, {
+        "date": "2020-03-03",
+        "mood": ["sad"],
+        "visual": "",
+        "prompt": "free_entry",
+        "entry": "Got a C on my test. Spent an hour at the gym trying to work off  the frustration. I'll do better next time."
+    }, {
+        "date": "2020-03-01",
+        "mood": ["happy"],
+        "visual": "",
+        "prompt": "free_entry",
+        "entry": "It was sunny today, and I went for a walk. It's nice to feel winter passing. Maybe I'll get out more now."
+    }, {
+        "date": "2020-02-04",
+        "mood": [],
+        "visual": "",
+        "prompt": "free_entry",
+        "entry": "Went to a move night today"
+    }, {
+        "date": "2020-01-01",
+        "mood": ["apprehensive"],
+        "visual": "",
+        "prompt": "free_entry",
+        "entry": "Happy New Year! Going back to school soon."
+    }]
 }];
